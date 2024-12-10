23de4c286997dc14120b57c4d17226380169eced
erl-j <nicolasjonason@gmail.com>
2024-12-10 00:31:32 +0000

    better instructions

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ with demo:
     # Erl-j's Soundfont Generator.
     Generate soundfonts from text descriptions using latent flow matching. You can then download the complete SFZ soundfont package to use the instrument locally.
     ## Instructions
-    1. Enter a text prompt to describe the audio you want to generate.
+    1. Enter a text prompt to describe the audio you want to generate. For example, "hard bass" or "dark analog lead flute flute flute soft".
     2. Adjust the number of generation steps to tradeoff between quality and speed (kindof).
     3. Click the "Generate Soundfont" button to generate the audio and soundfont.
     4. Preview the generated instrument with the keyboard.
